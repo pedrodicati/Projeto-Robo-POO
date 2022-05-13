fa1a2b361ecc2795683482c0170b26ee1cb8a477
Pedro Dicati <phgedi48@gmail.com>
2022-05-13 11:49:38 -0300

    alterações necessárias por conta do erro de importação circular

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-<div style="width: inline;">
+<div style="width: inline; text-align: center; padding: 10px;">
     <img style="width: 100px;" src="http://peteletrica.uff.br/wp-content/uploads/sites/509/2021/09/Python-logo-notext.svg_.png">
     <img style="width: 100px;" src="https://paulogala.com.br/wp-content/uploads/2018/08/robo.png">
 </div>
+
+<p style="text-align: center;">  
+  <img src="http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=GREEN&style=for-the-badge"/>
+</p>
 
 # Projeto robo POO
 <p>Projeto de um jogo de robôs desenvolvido para obtenção de nota no componente de Programação Orientada a Objetos, implementado em python.</p>
--- main.py
+++ main.py
@@ -1,6 +1,74 @@
-from robot import Robot
-from robotFighter import fighterRobot
-from robotMedical import medicalRobot
+from typing import List
+from robot import Robot, fighterRobot, medicalRobot
+# from robotFighter import fighterRobot
+# from robotMedical import medicalRobot
+
+def criaRobo() -> None:
+    print("Existem 3 tipos de robôs:")
+    print("1 - Robô")
+    print("2 - Robô Lutador")
+    print("3 - Robô Médico")
+
+    tipo = int(input("Digite o número do tipo do robô que deseja criar: "))
+
+    if tipo == 1:
+        print("Tipo escolhido: ROBÔ")
+        name = str(input("Digite o nome do sue novo robô: "))
+        listaRobots.append(Robot(name))
+    elif tipo == 2:
+        print("Tipo escolhido: ROBÔ LUTADOR")
+        name = str(input("Digite o nome do sue novo robô: "))
+        listaRobots.append(fighterRobot(name))
+    elif tipo == 3:
+        print("Tipo escolhido: ROBÔ MÉDICO")
+        name = str(input("Digite o nome do sue novo robô: "))
+        listaRobots.append(medicalRobot(name))
+
+def listaRobot() -> None:
+    for i in range(len(listaRobots)):
+        print(f"ID: {i}\n{listaRobots[i]}\n")
 
 if __name__ == "__main__":
-    pass
+    conditionWhile = True
+    listaRobots = list()
+    
+    m = Robot("l")
+    a = fighterRobot('trot')
+    b = medicalRobot('saosao')
+    listaRobots.append(m)
+    listaRobots.append(a)
+    listaRobots.append(b)
+    
+    while (conditionWhile == True):
+        listaRobot()
+        print("MENU")
+        print("1 - Criar robôs")
+        print("2 - Lista robôs")
+        print("3 - Casamento de robôs (gera um filho robô)")
+        print("4 - Luta")
+        print("0 - Sair do programa")
+
+        menu = int(input("Digite o número da operação que deseja realizar: "))
+
+        if menu == 1:
+            criaRobo()
+        elif menu == 2:
+            listaRobot()
+        elif menu == 3:
+            pass
+        elif menu == 4:
+            pass
+        elif menu == 0:
+            conditionWhile = False
+    
+    
+
+    # print(len(listaRobots))
+    # m = fighterRobot('fg')
+    # listaRobots.append(fighterRobot('s'))
+    # listaRobot()
+
+    # print(listaRobots[3])
+
+    criaRobo()
+    listaRobot()
